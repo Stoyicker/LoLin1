d45aa1d2e3a715e1bdd826e841e6d0c124dfd351
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-12-13 17:08:34 +0100

    First final version.

--- app/src/main/java/org/jorge/lolin1/service/NewsFeedHarvestService.java
+++ app/src/main/java/org/jorge/lolin1/service/NewsFeedHarvestService.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 
 public class NewsFeedHarvestService extends FeedHarvestService {
 
+    @SuppressWarnings("FieldCanBeLocal")
+    private final String NEWS_URL_TEMPLATE = "http://lolin1-feed-parser.herokuapp" +
+            ".com/services/news?realm=%s&locale=%s";
+
     public NewsFeedHarvestService() {
         super(NewsFeedHarvestService.class.getName());
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        //TODO Send the right data
-        intent.putExtra(EXTRA_SOURCE_URL, "http://feed43.com/lolnews_euw_en.xml");
-        intent.putExtra(EXTRA_TABLE_NAME, "NEWS_EUW_EN_US");
+        final String upperCaseRealm = intent.getStringExtra(EXTRA_REALM),
+                upperCaseLocale = intent.getStringExtra(EXTRA_LOCALE);
+        intent.putExtra(EXTRA_SOURCE_URL, String.format(NEWS_URL_TEMPLATE, upperCaseRealm,
+                upperCaseLocale));
+        intent.putExtra(EXTRA_TABLE_NAME, "NEWS_" + upperCaseRealm + "_" + upperCaseLocale);
         super.onHandleIntent(intent);
     }
 }
